cae881b47ced0bbf614cd31e4a4018f6455e67cd
ganesh-swami <ganeshswami018@gmail.com>
2024-05-16 16:04:14 +0530

    build error fixes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/utils/nextAuthConfig"
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- src/utils/nextAuthConfig.ts
+++ src/utils/nextAuthConfig.ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
+import {AuthOptions} from "next-auth";
 import { Account, User as AuthUser } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import User from "@/models/User";
 import connect from "@/utils/db";
 
-export const authOptions: any = {
+export const authOptions: AuthOptions = {
     // Configure one or more authentication providers
     providers: [
       CredentialsProvider({
@@ -40,11 +40,11 @@ export const authOptions: any = {
       strategy: "jwt",
     },
     callbacks: {
-      async signIn({ user, account }: { user: AuthUser; account: Account }) {
-        if (account?.provider == "credentials") {
-          return true;
-        }
-      },
+      // async signIn({ user, account }: { user: AuthUser; account: Account }) {
+      //   if (account?.provider == "credentials") {
+      //     return true;
+      //   }
+      // },
       // async jwt({ token, account }) {
       //   // Persist the OAuth access_token to the token right after signin
       //   console.log("token ========================== ",token, account);
